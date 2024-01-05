cefaa2f3b249a6ab778253050ebf0860599ea57e
SwiftFc <aseiduhenrydade@gmail.com>
2024-01-04 20:50:53 -0800

    fix

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,20 +1,3 @@
 #!/usr/bin/env ruby
-
-def match_school(input)
-  regex = /School/
-
-  if input =~ regex
-    puts "The input '#{input}' matches the pattern 'School'."
-  else
-    puts "The input '#{input}' does not match the pattern 'School'."
-  end
-end
-
-if ARGV.empty?
-  puts "Please provide an argument."
-else
-  input_argument = ARGV[0]
-
-  match_school(input_argument)
-end
-
+# Ruby script that accepts one argument
+puts ARGV[0].scan(/School/).join
